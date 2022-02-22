0febfa0c1a53124504c54528cd42108382b2822b
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-02-22 13:15:10 -0300

    Dia 1
    Parte 2 do dia dei uma pausa pra pensar no que to fazendo

---
+++ Projeto/whatsapp-2/components/sidebar.js
@@ -0,0 +1,15 @@
+import styles from 'styled-components'
+function Sidebar(){
+    return(
+        <Container>
+            <h1>SideBar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div(
+
+
+);
--- Projeto/whatsapp-2/pages/chat.js
+++
@@ -1,9 +0,0 @@
-function chat(){
-    return(
-        <div>
-            <h1>Essa é a pagina de chat</h1>
-        </div>
-    );
-}
-
-export default chat
